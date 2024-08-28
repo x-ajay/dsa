e87eafcb184b286eadbac32b15816263e43121b4
Ajay Kumbhar <ajayakumbhar1234@gmail.com>
2024-08-28 14:55:18 +0530

    exclude code folder

--- code/main.go
+++ code/main.go
@@ -1,14 +1,5 @@
 package main
 
-import "fmt"
-
-func numberOfWay(n, m int) int {
-	if n == 1 || m == 1 {
-		return 1
-	}
-	return numberOfWay(n-1, m) + numberOfWay(n, m-1)
-}
-
 func main() {
-	fmt.Println(numberOfWay(10, 10))
+
 }
